04d50a2a84c2b2dda6df9e4592072f4e38dac96b
yuyu0830 <yuyu0830@naver.com>
2023-07-31 00:05:27 +0900

    23-07-31

--- solved/1351.cpp
+++ solved/1351.cpp
@@ -1,4 +1,3 @@
-// DP 골드 5 무한수열 https://www.acmicpc.net/problem/1351
 #include <iostream>
 #include <queue>
 #include <algorithm>
@@ -10,9 +9,9 @@
 using namespace std;
 
 typedef struct ii {
-    int x;
-    long long int y;
-    ii(int X, long long int Y) : x(X), y(Y) {};
+    int ptr;
+    long long int v;
+    ii(int X, long long int V) : ptr(X), v(V) {};
     ii* next = nullptr;
 };
 
@@ -20,11 +19,11 @@ long long int n;
 int p, q;
 ii* dp[NUM + 1] = { nullptr, };
 
-long long int find(int t, int n) {
-    if (dp[n] != nullptr) {
-        ii* tmp = dp[n];
+long long int find(int front, int back) {
+    if (dp[back] != nullptr) {
+        ii* tmp = dp[back];
         while (tmp) {
-            if (tmp->x == t) return tmp->y;
+            if (tmp->ptr == front) return tmp->v;
             else tmp = tmp->next;
         }
     }
@@ -32,24 +31,24 @@ long long int find(int t, int n) {
 }
 
 void push(long long int p, long long int v) {
-    int t = p / NUM;
-    int tp = p % NUM;
+    int front = p / NUM;
+    int back = p % NUM;
 
-    ii* a = new ii(t, v);
+    ii* a = new ii(front, v);
 
-    if (!dp[tp]) dp[tp] = a;
+    if (!dp[back]) dp[back] = a;
     else {
-        ii* tmp = dp[tp];
+        ii* tmp = dp[back];
         while (tmp->next) tmp = tmp->next;
         tmp->next = a;
     }
 }
 
 long long int f(long long int n) {
-    int t = n / NUM;
-    int tn = n % NUM;
+    int front = n / NUM;
+    int back = n % NUM;
 
-    long long int ret = find(t, tn);
+    long long int ret = find(front, back);
     if (ret) return ret;
 
     long long int tp = n / p;
---
+++ solved/2011.cpp
@@ -0,0 +1,41 @@
+// DP 골드 5 암호코드
+#include <iostream>
+#include <string>
+#include <queue>
+#include <algorithm>
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+#define MAX 999999999
+
+using namespace std;
+
+int dp[5002] = { 0, };
+
+int main() {
+    fastio;
+    string s; cin >> s;
+    s = "0" + s;
+
+    dp[0] = 1;
+    for (int i = 1; i < s.size(); i++) {
+        if (s[i] == 48) {
+            if (s[i - 1] == 49 || s[i - 1] == 50) {
+                dp[i] = dp[i - 2] % 1000000;
+            }
+            else if (s[i - 1] > 50 || s[i - 1] == 48) {
+                printf("0\n");
+                return 0;
+            }
+        }
+        else {
+            if (s[i - 1] == 49 || (s[i - 1] == 50 && s[i] <= 54)) {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000;
+            }
+            else dp[i] = dp[i - 1] % 1000000;
+        }
+    }
+
+    printf("%d\n", dp[s.size() - 1]);
+}
+
